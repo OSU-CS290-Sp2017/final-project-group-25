e7d3884318b1631e8da8cd773c2bfe6e4c6a47ea
thadenf <thadenf@oregonstate.edu>
2017-06-10 21:11:20 -0700

    Delete index.js

--- public/index.js
+++
@@ -1,6 +0,0 @@
-/*
- * Add the contents of your index.js file from Assignment 3 here to see the
- * interactions you implemented.
- */
-
-alert('Add the content of your index.js from Assignment 3 to public/index.js.');
