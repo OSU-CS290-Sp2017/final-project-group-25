0b7c9ddab8913892956c348a1bb19d9c0d26566e
Sean Cramsey <cramseys@oregonstate.edu>
2017-06-15 17:13:51 -0700

    Reads/Writes to json

--- server.js
+++ server.js
@@ -1,7 +1,59 @@
+var path = require('path');
+var fs = require('fs');
+var express = require('express');
+var exphbs = require('express-handlebars');
+var bodyParser = require('body-parser');
+
+var postData = require('./posts');
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.use(bodyParser.json());
+
+app.get('/$', function (req, res, next) {
+
+  var templateArgs = {
+    post: postData,
+  };
+  res.render('index', templateArgs);
+});
+
+app.post('/posts/addListing', function (req, res, next) {
+  var posts = postData
+  var posting = {
+    text: req.body.text,
+    amount: req.body.amount,
+    imgUrl: req.body.imgUrl
+  };
+
+  posts.push(posting);
+  fs.writeFile('posts.json', JSON.stringify(postData), function (err) {
+    if (err) {
+      res.status(500).send("Unable to save listing to \"database\".");
+    } else {
+      res.status(200).send();
+    }
+  });
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('*', function (req, res) {
+  res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
+});
+// Start the server listening on the specified port.
+app.listen(port, function () {
+  console.log("== Server listening on port", port);
+});
+
 /*
  * CS290
  * Frank Thaden
  */
+/*
 var http = require("http");
 var connect = require('connect');
 var fs = require('fs');
@@ -51,3 +103,43 @@ function requestHandler(request, responce)
 
 http.createServer(requestHandler).listen(3000);
 console.log("running.....................");
+*/
+/*
+var path = require('path');
+var fs = require('fs');
+var express = require('express');
+var exphbs = require('express-handlebars');
+var bodyParser = require('body-parser');
+var MongoClient = require('mongodb').MongoClient;
+
+var app = express();
+var port = process.env.PORT || 3000;
+
+var mongoHost = process.env.MONGO_HOST;
+var mongoPort = process.env.MONGO_PORT || 27017;
+var mongoUser = process.env.MONGO_USER;
+var mongoPassword = process.env.MONGO_PASSWORD;
+var mongoDBName = process.env.MONGO_DB;
+var mongoURL = 'mongodb://' + mongoUser + ':' + mongoPassword +
+  '@' + mongoHost + ':' + mongoPort + '/' + mongoDBName;
+var mongoDB;
+
+console.log('== MongoDB URL:', mongoURL);
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.use(bodyParser.json());
+
+
+// Start the server listening on the specified port.
+MongoClient.connect(mongoURL, function (err, db) {
+  if (err) {
+    throw err;
+  }
+  mongoDB = db;
+  app.listen(port, function () {
+    console.log("== Server listening on port", port);
+  });
+});
+*/
